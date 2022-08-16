b0970355fcb480493b9bc7decdc1e2ffd8c09fa4
Jonas Pammer <opensource@jonaspammer.at>
2022-08-16 17:22:45 +0200

    chore: turn critical overwrite info log into notice log
    verboselog!

--- __scripts/ci.py
+++ __scripts/ci.py
@@ -105,7 +105,7 @@ def main() -> int:
 
     # Create pristine branch
     if PR_BRANCH in execute(["git", "branch", "--list"], repo_path):
-        logger.info(
+        logger.notice(
             f"Branch {PR_BRANCH} already exists. "
             f"Note that this script will force-overwrite it "
             f"to accomodate potentially changed script behaviour."
