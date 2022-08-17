0c084e7f6284568c7ab1d5d48f3e863009edf3fe
Jonas Pammer <opensource@jonaspammer.at>
2022-08-17 13:42:33 +0200

    chore: add markdown highlights to pr description

--- __scripts/ci.py
+++ __scripts/ci.py
@@ -128,12 +128,12 @@ def main() -> int:
     )
     if len(tags_since) > 1:
         DESCRIPTION += (
-            f"NOTE: There have been {len(tags_since)} new versions since "
+            f"\nNOTE: There have been **{len(tags_since)}** new versions since "
             f"{current_checkmk_server_version}. "
             f"After this PR has been merged, the github workflow will run again "
             f"and a new PR will open semi-immideatily. "
-            f"Please ensure to create a proper tag/release "
-            f"after and for every merged ci.py PR."
+            f"Please **ensure to create a proper tag/release "
+            f"for every merged ci.py PR**."
         )
     SCRIPT_MSG: str = (
         "Authored by `__scripts/ci.py` python script"
