145976a563efe7570129c84224560f3a47252615
Jonas Pammer <opensource@jonaspammer.at>
2022-08-18 17:37:57 +0200

    chore: add verbose log for get_all_remote_sums

--- __scripts/utils.py
+++ __scripts/utils.py
@@ -103,6 +103,10 @@ def get_all_remote_sums(checkmk_server_version: str) -> dict[str, str]:
         of the release in question (which may be the literal "None").
     """
     checkmk_server_version = checkmk_server_version.replace("v", "")
+    logger.verbose(
+        "Fetching checksums of every distribution. "
+        "This may take a while, as we need to download the files!"
+    )
     results = {}
     for distro, releases in DEBIAN_DISTROS.items():
         logger.debug(distro + " " + str(releases))
