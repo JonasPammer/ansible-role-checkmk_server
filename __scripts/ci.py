2f12a0cd250a46b0df6bb06ed1f7a96ec5caf0e5
Jonas Pammer <opensource@jonaspammer.at>
2022-08-22 11:20:30 +0200

    refactor: use GITHUB_TOKEN even if dry_run'ing
    the reason i've added this now removed behaviour was without token the API obviously can not create/do stuff.
    dry_run checks are now performed before the actual github api actions, so we can remove this
    and take advantage of the other reason to add a token: api limit increase

--- __scripts/ci.py
+++ __scripts/ci.py
@@ -484,7 +484,7 @@ def main() -> None:
     args = parser.parse_args()
     init_logger(args.verbose, args.silent)
 
-    _login_or_token = None if args.dry_run else os.environ["GITHUB_TOKEN"]
+    _login_or_token = os.environ["GITHUB_TOKEN"]
     github_api: Github = Github(_login_or_token)
 
     server_repo: Repository = github_api.get_repo(
